1afa5b9192fb46be0a33298b68d7eedbcc52cc9d
wickedsick771 <kotkaboec@gmail.com>
2016-11-23 02:45:02 +0200

    s

---
+++ blog/src/main/java/softuniBlog/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package softuniBlog.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import softuniBlog.entity.Article;
+
+
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+}
